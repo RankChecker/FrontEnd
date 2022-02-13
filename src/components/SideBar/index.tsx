7e54711e9d82996f0a0853460ddeebc9311eefc2
wueliton <paulo.wueliton@hotmail.com>
2022-02-13 15:10:14 -0300

    Modificado menu

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
-import ManageSearchIcon from '@mui/icons-material/ManageSearch';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Toolbar from '@mui/material/Toolbar';
-import { Header } from '../Header';
-import { FormSearch } from '../FormSearch';
-import { CardRanking } from '../CardRanking';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import ManageSearchIcon from "@mui/icons-material/ManageSearch";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import { Header } from "../Header";
+import { FormSearch } from "../FormSearch";
+import { CardRanking } from "../CardRanking";
 import style from "./sideBar.module.scss";
 import logo from "../../assets/logo.svg";
-import Image from 'next/image'
+import Image from "next/image";
 const drawerWidth = 240;
 
 interface Props {
@@ -34,33 +34,31 @@ export function ResponsiveDrawer(props: Props) {
 
   const drawer = (
     <div className={style.menuBar}>
-      {<div className={style.logoMenuBar}>
-        <Image
-          src={logo}
-          alt="Logo"
-          width={36}
-          height={36}
-        />
-        <span>ANÁLISE</span>
-      </div>}
-      
+      {
+        <div className={style.logoMenuBar}>
+          <Image src={logo} alt="Logo" width={36} height={36} />
+        </div>
+      }
+
+      <span>ANÁLISE</span>
       <List className={style.menuBarList}>
-        {['Busca de Palavra'].map((text, index) => (
+        {["Busca de Palavra"].map((text, index) => (
           <ListItem button key={text} className={style.active}>
             <ListItemIcon className={style.itemIconMenuBar}>
-              <ManageSearchIcon className={style.iconMenuBar}/>
+              <ManageSearchIcon className={style.iconMenuBar} />
             </ListItemIcon>
-            <ListItemText primary={text}/>
+            <ListItemText primary={text} />
           </ListItem>
         ))}
       </List>
     </div>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }} className={style.sideBar}>
+    <Box sx={{ display: "flex" }} className={style.sideBar}>
       <CssBaseline />
       <Header
         drawerWidth={drawerWidth}
@@ -71,7 +69,6 @@ export function ResponsiveDrawer(props: Props) {
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-
         <Drawer
           container={container}
           variant="temporary"
@@ -81,8 +78,11 @@ export function ResponsiveDrawer(props: Props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -90,8 +90,11 @@ export function ResponsiveDrawer(props: Props) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
@@ -100,7 +103,11 @@ export function ResponsiveDrawer(props: Props) {
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         <Toolbar />
         <FormSearch />
@@ -108,7 +115,6 @@ export function ResponsiveDrawer(props: Props) {
         <span className={style.titleSection}>Gerando Relatório</span>
         <CardRanking />
         <CardRanking />
-
       </Box>
     </Box>
   );
