8e16d1a47f5c8553e438ec0440b1035208640929
wueliton <paulo.wueliton@hotmail.com>
2022-02-14 09:25:49 -0300

    Removida importação do ResponsiveDrawer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { Dashboard } from "../components/Dashboard";
-import { ResponsiveDrawer } from "../components/SideBar";
 export default function Home() {
   return (
     <>
